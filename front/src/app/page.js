3df8a14bca7698343de680b7097e9540e259b0c5
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2023-10-09 13:59:50 -0300

    PROJETO SEM NADA

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,144 +1,113 @@
-'use client'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-import finance from './finance.png'
-import { useEffect, useState } from 'react'
-import carro from './carro.png'
-import iconefinance from './iconefinance.png'
-import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 
 export default function Home() {
-  useEffect(()=>{
-
-  })
-
-  const router = useRouter()
-
-  const [months,setMonths] = useState('')
-  const [value, setValue] = useState('')
-
-
-  async function handleSubmit(e) {
-    e.preventDefault()
-
-    await fetch("http://localhost:3001/clear")
-    .then(e=>{
-      console.log('Limpado')
-    })
-
-    const valor = Number(value)
-    if (valor <= 0 || isNaN(valor)) {
-      setValue('0')
-      return alert("Valor inválido")
-    }
-
-    if (!months) {
-      return alert("Preencha todos os campos ")
-    }
-    
-    router.push(`/emprestimo?valorPedido=${value}&numParcelas=${months}`);
-
-
-    setMonths('')
-    setValue('')
-  }
-
   return (
-    <div className='flex justify-around w-full h-screen items-center max-[1220px]:bg-green-100'>
-      <div className='max-[1220px]:hidden fixed justify-center items-center border-l-2 rounded-full p-6'>
-        <div className='bg-white w-16 py-3 rounded-full shadow-2xl'>
-          <MdOutlineKeyboardArrowRight className='mx-auto text-gray-600 text-4xl' />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.js</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
         </div>
       </div>
-      <div className='w-1/2 max-[1220px]:hidden'>
-        <header className='flex flex-col justify-center items-center'>
-          <div className='text-start w-80'>
-            <Image
-              src={finance}
-              alt='Logo finance'
-              width={163}
-              height={37}
-            />
-          </div>
-          <div className='flex flex-col justify-center items-center mt-12'>
-            <h1 className='text-start w-80 text-gray-600 text-4xl font-semibold'>
-              Simule o seu
-            </h1>
-            <h1 className='text-start w-80 text-4xl font-semibold text-green-300'>
-              financiamento<span className='text-gray-600'>.</span>
-            </h1>
-            <p className='text-start text-gray-400 mt-4 w-80'>
-              Escolha as melhores condições para o financiamento do seu veículo.
-            </p>
-          </div>
-          <div className='mt-16'>
-            <Image
-            src={carro}
-            width={594}
-            height={285}
-            alt='Carro'
-            />
-          </div>
-        </header>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
       </div>
-      <div className='w-1/2 bg-green-100 h-screen flex flex-col justify-center items-center'>
-        <header className='flex flex-col items-center justify-center min-[1220px]:items-start min-[1220px]:w-96'>
-          <div>
-            <Image
-            src={finance}
-            alt='Logo finance'
-            width={163}
-            height={37}
-            className='max-[1220px]:flex hidden'
-            />
-            <Image
-            src={iconefinance}
-            alt='Logo finance'
-            width={79}
-            height={51}
-            className='max-[1220px]:hidden flex'
-            />
-          </div>
-          <div className='flex flex-col justify-center items-center mt-12'>
-            <h1 className='text-gray-600 text-4xl font-semibold max-[1220px]:flex hidden'>
-              Simule o seu
-            </h1>
-            <h1 className='text-4xl font-semibold text-green-300 max-[1220px]:flex hidden'>
-              financiamento<span className='text- text-gray-600'>.</span>
-            </h1>
-            <p className='w-96 max-[1220px]:text-center max-[1220px]:w-54 text-gray-400 mt-4'>
-              Preencha os campos e simule o seu financiamento.
-            </p>
-          </div>
-        </header>
-        <main>
-          <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center gap-3'>
-            <input
-            type='number'
-            placeholder='Valor'
-            value={value}
-            onChange={({ target }) =>{
-              setValue((target.value))
-            }}
-            className='border border-gray-300 w-96 h-10 mt-12 px-4 rounded-md max-[420px]:w-80'
-            />
-            <select value={months} onChange={({ target }) => setMonths(target.value)} className='w-96 max-[420px]:w-80 py-2 px-3 border border-gray-300 rounded-md text-gray-500'>
-              <option value="" disabled>
-                Quantos meses?
-              </option>
-              <option className='text-black' value="12">12</option>
-              <option className='text-black' value="24">24</option>
-              <option className='text-black' value="36">36</option>
-              <option className='text-black' value="48">48</option>
-              <option className='text-black' value="60">60</option>
-              <option className='text-black' value="72">72</option>
-            </select>
-            <button onClick={()=>handleSubmit} style={{fontWeight:'bold'}} type='submit' className='bg-green-400 text-white font-smibold w-96 h-10 rounded-md max-[420px]:w-80'>
-              Simular financiamento
-            </button>
-          </form>
-        </main>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore the Next.js 13 playground.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
       </div>
-    </div>
+    </main>
   )
 }
