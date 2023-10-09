8a04eea1d8e3cadb9efb2469da6c5f7adaa772c8
Khalel Albuquerque <khaleledipo@gmail.com>
2023-10-09 13:16:00 -0300

    contoller and bank route

--- back/controllers/bankController.js
+++ back/controllers/bankController.js
@@ -2,9 +2,14 @@ const Bank = require('../models/Bank')
 
 
 function calcJuros(valorEntrada, anualTax, months) {
-    const decimalTax = anualTax / 100;
-    const total = valorEntrada * Math.pow(1 + decimalTax, months);
-    return total;
+    const jurosDec = anualTax / 100
+    const txJurMensal = jurosDec / 12
+    const valor = valorEntrada * Math.pow(1 + txJurMensal, months )
+    
+    const valorFinal = valor.toFixed(2)
+    
+    return parseFloat(valorFinal)
+        
 }
 
 
@@ -37,20 +42,18 @@ module.exports = class BankController{
             if(!banks) return res.status(404).json({message: "No bank found"})
 
             async function processBank(bank) {
-                const valorComJuros = await calcJuros(valorPedido, bank.anual_int, numParcelas).toFixed(2);
+
+                const valorComJuros = calcJuros(valorPedido, bank.anual_int, numParcelas).toFixed(2);
                 const valorMensal = (valorComJuros / numParcelas).toFixed(2);
 
                 bank.finalValue = parseFloat(valorComJuros);
                 bank.mensalValue = parseFloat(valorMensal);
-
-              
                 await bank.save()
             }
               
-            for (const bank of banks) {
+            for (let bank of banks) {
                 await processBank(bank)
             }
-            
 
             return res.status(200).json({'success': banks})
         }catch(err){
@@ -71,6 +74,23 @@ module.exports = class BankController{
         }catch(err){
             return res.status(500).json({message: err.message})
         }
+    }
+
+    static async clearBanks(req,res, next){
+        try {
+            const allBanks = await Bank.find();
+          
+            for (const bank of allBanks) {
+                bank.mensalValue = 0;
+                bank.finalValue = 0;
+                await bank.save();
+              }
+
+            next()
+          } catch (err) {
+            return res.status(500).json({ message: err.message });
+          }
+          
     }
 
 }
